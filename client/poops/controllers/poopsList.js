256194923def4cdbcbf85d9022d7004e2f53fc33
mrisoli <celorisoli@gmail.com>
2015-11-19 22:33:19 -0200

    Adding arrow funcionts to poopsListCtrl

--- client/poops/controllers/poopsList.js
+++ client/poops/controllers/poopsList.js
@@ -1,7 +1,7 @@
 angular.module('poopsquare').controller('PoopsListCtrl', function ($scope, $meteor) {
     $scope.poops = $meteor.collection(Poops);
 
-    $scope.remove = function(poop){
-      $scope.poops.splice($scope.poops.indexOf(poop), 1);
-    };
+    $scope.remove = (poop) => $scope.poops.remove(poop);
+
+    $scope.removeAll = () => $scope.parties.remove();
   });
