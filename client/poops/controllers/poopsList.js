b0f922cba8b6d7c1e4acf2f04ca2a3f7ef4161c3
mrisoli <celorisoli@gmail.com>
2015-11-19 22:30:01 -0200

    Fixing poopCtrl

--- client/poops/controllers/poopsList.js
+++ client/poops/controllers/poopsList.js
@@ -1,3 +1,7 @@
 angular.module('poopsquare').controller('PoopsListCtrl', function ($scope, $meteor) {
-    $$scope.poops = $meteor.collection(Poops);
+    $scope.poops = $meteor.collection(Poops);
+
+    $scope.remove = function(poop){
+      $scope.poops.splice($scope.poops.indexOf(poop), 1);
+    };
   });
